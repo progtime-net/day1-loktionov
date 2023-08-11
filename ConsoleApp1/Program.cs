11390962026633515158aa5cfa571f03ca002d53
Timofey06 <tima.loktionov@yandex.ru>
2023-08-11 13:11:35 +0300

    greetings_init

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,5 +3,8 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("What's your name?");
+        var name=Console.ReadLine();
+        Console.WriteLine("Hellow, "+ name+ "!");
     }
 }
